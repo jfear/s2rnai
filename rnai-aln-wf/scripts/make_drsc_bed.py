d317e3eb34e1bf6d9fc73f352415e7f86f15d729
Justin M. Fear <justin.m.fear@gmail.com>
2020-01-14 10:28:20 -0500

    Refactors to update strand info.Lee thinks the strand should always be the sam as the target gene. I refactored script to make this update.

--- rnai-aln-wf/scripts/make_drsc_bed.py
+++ rnai-aln-wf/scripts/make_drsc_bed.py
@@ -14,68 +14,128 @@ BED formatted file ../output/drsc_coordinates.bed
 import gzip
 import os
 import urllib.request
+import tempfile
+from pathlib import Path
 from typing import Generator, Tuple, List
+from collections import namedtuple
 
 import pandas as pd
 
 from s2rnai.io import GffRow
 
+BedRecord = namedtuple("BedRecord", "chrom,start,end,name,score,strand")
 
 def main():
-    coords = [parse_gff_row(row) for row in download_gff() if "DRSC_dsRNA" in row]
-    coords.extend(add_missing())
+    gff_file_name = download_gff()
+    drsc_bed, gene_strand = split_gff(gff_file_name)
+    delete_tempfile(gff_file_name)
 
-    # Check for missing DRSCs
-    drscs_in_gff = {x[3] for x in coords}
-    drscs_in_study = get_drscs_in_study()
-    missing = sorted(drscs_in_study.difference(drscs_in_gff))
+    study_drsc_to_fbgn = (
+        pd.read_table(snakemake.input[0], usecols=["drsc", "target_FBgn"])
+        .drop_duplicates()
+        .query("drsc != 'DRSCNA'")
+        .set_index("drsc")
+        .squeeze()
+        .to_dict()
+    )
 
-    if len(missing) != 0:
-        raise ValueError("Missing the following DRSCs:\n%s" % "\n".join(missing))
+    study_drsc = pull_study_drsc(drsc_bed, study_drsc_to_fbgn.keys())
+    updated_drsc = update_drsc_strand(study_drsc, gene_strand, study_drsc_to_fbgn)
 
     # Write out table
-    pd.DataFrame(coords).to_csv(snakemake.output[0], sep="\t", index=False, header=False)
+    pd.DataFrame(updated_drsc).to_csv(snakemake.output[0], sep="\t", index=False, header=False)
 
 
-def download_gff() -> Generator[str, None, None]:
-    """Download and decompress GFF from FlyBase."""
-    with urllib.request.urlopen(snakemake.params.url) as response:
-        for line in gzip.decompress(response.read()).decode().splitlines():
-            yield line
+def download_gff() -> str:
+    gff_tempfile = get_tempfile_name()
+    response = urllib.request.urlopen(snakemake.params.url)
+    with open(gff_tempfile, "w") as file_out:
+        file_out.write(gzip.decompress(response.read()).decode())
+    return gff_tempfile
 
 
-def parse_gff_row(row: str) -> Tuple[str]:
-    """Parses GFF row into a tuple."""
-    gff = GffRow(row)
-    return gff.seqid, gff.start, gff.end, gff.parsed_attributes["Name"], gff.score, gff.strand
+def get_tempfile_name() -> str:
+    gff_obj = tempfile.NamedTemporaryFile(suffix=".gff", delete=False)
+    gff_file_name = gff_obj.name
+    gff_obj.close()
+    return gff_file_name
 
 
-def get_drscs_in_study() -> set:
-    """Pull out DRSCs in the study."""
-    return set(pd.read_csv(snakemake.input[0], sep="\t").query('drsc != "DRSCNA"').drsc.unique())
+def delete_tempfile(file_name):
+    Path(file_name).unlink()
 
 
-def add_missing() -> List[Tuple[str]]:
-    """Add missing drsc coordinates.
+def split_gff(gff_file_name) -> Tuple[List[BedRecord], dict]:
+    """Pulls out drsc and gene strand info"""
+    drsc = missing_drsc()
+    gene_strands = {}
+    with open(gff_file_name) as fh:
+        for row in fh:
+            if "DRSC_dsRNA" in row:
+                gff = GffRow(row)
+                drsc.append(
+                    BedRecord(
+                        gff.seqid,
+                        gff.start,
+                        gff.end,
+                        gff.parsed_attributes["Name"],
+                        gff.score,
+                        gff.strand,
+                    )
+                )
+            elif "FlyBase\tgene" in row:
+                gff = GffRow(row)
+                gene_strands[gff.parsed_attributes["ID"]] = gff.strand
+    return drsc, gene_strands
 
-    I compared these coordinates to a list of DRSC reagents used and found some to
-    be missing. I used the UP-TORR website to look up these reagent locations. I
-    add them here.
-    """
+
+def missing_drsc() -> List[BedRecord]:
+    """There are a handful of DRSC missing from the GFF"""
     return [
-        ("4", "545545", "545879", "DRSC38683", ".", "+"),
-        ("X", "6266464", "6266610", "DRSC40082", ".", "+"),
-        ("2L", "10264095", "10264265", "DRSC40331", ".", "+"),
-        ("3R", "27918291", "27918881", "DRSC40340", ".", "+"),
-        ("X", "7671912", "7672385", "DRSC40438", ".", "+"),
-        ("2L", "16698174", "16698323", "DRSC40694", ".", "+"),
-        ("X", "2448482", "2448642", "DRSC40820", ".", "+"),
-        ("2L", "15116373", "15116962", "DRSC40850", ".", "+"),
-        ("2R", "21143622", "21143953", "DRSC40918", ".", "+"),
-        ("3R", "25025517", "25025716", "DRSC41040", ".", "+"),
-        ("2R", "12633620", "12633819", "DRSC41571", ".", "+"),
-        ("2L", "9762479", "9762549", "DRSC42501", ".", "+"),
+        BedRecord("4", "545545", "545879", "DRSC38683", ".", "+"),
+        BedRecord("X", "6266464", "6266610", "DRSC40082", ".", "+"),
+        BedRecord("2L", "10264095", "10264265", "DRSC40331", ".", "+"),
+        BedRecord("3R", "27918291", "27918881", "DRSC40340", ".", "+"),
+        BedRecord("X", "7671912", "7672385", "DRSC40438", ".", "+"),
+        BedRecord("2L", "16698174", "16698323", "DRSC40694", ".", "+"),
+        BedRecord("X", "2448482", "2448642", "DRSC40820", ".", "+"),
+        BedRecord("2L", "15116373", "15116962", "DRSC40850", ".", "+"),
+        BedRecord("2R", "21143622", "21143953", "DRSC40918", ".", "+"),
+        BedRecord("3R", "25025517", "25025716", "DRSC41040", ".", "+"),
+        BedRecord("2R", "12633620", "12633819", "DRSC41571", ".", "+"),
+        BedRecord("2L", "9762479", "9762549", "DRSC42501", ".", "+"),
     ]
+
+
+def pull_study_drsc(drsc_bed: List[BedRecord], drscs_in_study: List[str]) -> List[BedRecord]:
+    records_in_study = [
+        record
+        for record in drsc_bed
+        if record.name in drscs_in_study
+    ]
+    check_all_study_drsc_present(records_in_study, drscs_in_study)
+    return records_in_study
+
+
+def check_all_study_drsc_present(records: List[BedRecord], drscs_in_study: List[str]):
+    present_drsc = set([record.name for record in records])
+    study_set = set(drscs_in_study)
+    difference = study_set.difference(present_drsc)
+    if len(difference) > 0:
+        raise ValueError("There are missing DRSCs:\n%s" % "\n".join(difference))
+
+
+def update_drsc_strand(drsc: List[BedRecord], gene_strand: dict, drsc_to_fbgn: dict) -> List[BedRecord]:
+    updated_records = []
+    for record in drsc:
+        drsc_id = record.name
+        target_fbgn = drsc_to_fbgn[drsc_id]
+        mutable_record = list(record)
+        updated_strand = gene_strand[target_fbgn]
+        mutable_record[5] = updated_strand
+        updated_records.append(BedRecord(*mutable_record))
+    return updated_records
+
 
 
 if __name__ == "__main__":
